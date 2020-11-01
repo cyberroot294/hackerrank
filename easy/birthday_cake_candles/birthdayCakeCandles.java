c0bc8b896e1c15774dc93c88120062c62218069e
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2020-11-01 22:48:31 +0700

    add solution for birthday cake candles problem

---
+++ easy/birthday_cake_candles/birthdayCakeCandles.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+ * birthdayCakeCandles
+ */
+public class birthdayCakeCandles {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // panjang array / banyak elemen
+        int max = in.nextInt(); // asumsi nilai max ialah elemen pertama dari array
+        int counter = 0; // variabel untuk menampung nilai max yang sama
+        for (int i = 0; i < n - 1; i++) {
+            int temp = in.nextInt();
+            if (temp > max) {
+                max = temp;
+                counter = 0;
+            } else if (temp == max) {
+                counter++;
+            }
+        }
+        System.out.print(counter + 1); // ditambah dengan 1 karna nilai awal max belom dihitung ke counter
+
+    }
+}
