a167000e1d94f14d92e8298d9fa3afbf60a2e030
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2020-11-15 12:37:17 +0700

    add solution for 'breaking the records' problem

---
+++ easy/breaking-the-records/breakingTheRecords.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * breakingTheRecords
+ */
+public class breakingTheRecords {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int len = in.nextInt();
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        breakingTheRecords(arr);
+    }
+
+    public static void breakingTheRecords(int[] arr) {
+        int highestScore = arr[0]; // assume the highest score
+        int lowestScore = arr[0]; // assume the lowest score
+        int breakTheHighest = 0;
+        int breakTheLowest = 0;
+        for (int i : arr) {
+            if (i > highestScore) {
+                breakTheHighest++;
+                highestScore = i;
+            } else if (i < lowestScore) {
+                breakTheLowest++;
+                lowestScore = i;
+            }
+        }
+        System.out.printf("%d %d", breakTheHighest, breakTheLowest);
+    }
+}
